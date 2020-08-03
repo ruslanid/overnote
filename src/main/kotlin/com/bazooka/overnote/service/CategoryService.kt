4e43a2aa78bf3243954a36ce75cf5af77c017e22
unknown <ruslan.idrisov01@gmail.com>
2020-08-02 23:15:55 -0400

    Add category service

---
+++ src/main/kotlin/com/bazooka/overnote/service/CategoryService.kt
@@ -0,0 +1,42 @@
+package com.bazooka.overnote.service
+
+import com.bazooka.overnote.exception.ResourceNotFoundException
+import com.bazooka.overnote.model.Category
+import com.bazooka.overnote.model.Note
+import com.bazooka.overnote.repository.CategoryRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+import java.util.*
+
+@Service
+class CategoryService {
+
+  @Autowired
+  private lateinit var categoryRepository: CategoryRepository
+
+  fun findAll(): Iterable<Category> =
+          categoryRepository.findAll()
+
+  fun saveCategory(category: Category): Category =
+          categoryRepository.save(category)
+
+  fun findCategoryById(categoryId: Int): Category {
+      val result: Optional<Category> = categoryRepository.findById(categoryId)
+
+      if (result.isEmpty)
+        throw ResourceNotFoundException("Category with ID: $categoryId was not found")
+
+      return result.get()
+  }
+
+  fun updateCategoryById(categoryId: Int, newCategory: Category): Category {
+    val oldCategory = findCategoryById(categoryId)
+    val updatedCategory = oldCategory.copy(name = newCategory.name)
+    return saveCategory(updatedCategory)
+  }
+
+  fun deleteCategoryById(categoryId: Int) {
+    val category = findCategoryById(categoryId)
+    return categoryRepository.delete(category)
+  }
+}
